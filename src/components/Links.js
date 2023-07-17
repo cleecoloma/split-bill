20f226f74adbeb0733a4ca8252cd60a7ae2d4d22
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-16 21:41:18 -0700

    added links

---
+++ src/components/Links.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Links() {
+  return (
+    <div className="bg-slate-200 mx-auto flex justify-center items-center">
+      <a className="hover:bg-gray-300 py-3 px-5">Home</a>
+      <a className="hover:bg-gray-300 py-3 px-5">History</a>
+      <a className="hover:bg-gray-300 py-3 px-5">Friends</a>
+    </div>
+  );
+}
+
+export default Links
