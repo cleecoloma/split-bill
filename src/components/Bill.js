54875d2c4e73f319b56c6ff8a03965cece670b9b
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-16 23:06:21 -0700

    added table layout

--- src/components/Bill.js
+++ src/components/Bill.js
@@ -2,10 +2,18 @@ import React from 'react'
 
 function Bill() {
   return (
-    <div className='bg-gray-100 h-80 m-6 rounded-2xl'>
-      
+    <div className="bg-gray-100 h-80 m-6 rounded-2xl">
+      <table className='table-auto'>
+        <thead className="flex">
+          <tr>
+            <th className="w-1/2">Item</th>
+            <th className="w-1/4"># of Friends</th>
+            <th className="w-1/4">Amount</th>
+          </tr>
+        </thead>
+      </table>
     </div>
-  )
+  );
 }
 
 export default Bill
--- src/components/Links.js
+++ src/components/Links.js
@@ -3,13 +3,13 @@ import React from 'react'
 function Links() {
   return (
     <div className="bg-slate-200 mx-auto flex justify-center items-center">
-      <a className="hover:bg-[#008080] hover:text-white hover:rounded-lg flex justify-center py-3 w-24">
+      <a className="hover:bg-[#008080] hover:text-white hover:rounded-lg text-[#035454] flex justify-center py-3 w-24">
         Home
       </a>
-      <a className="hover:bg-[#008080] hover:text-white hover:rounded-lg flex justify-center py-3 w-24">
+      <a className="hover:bg-[#008080] hover:text-white hover:rounded-lg text-[#035454] flex justify-center py-3 w-24">
         History
       </a>
-      <a className="hover:bg-[#008080] hover:text-white hover:rounded-lg flex justify-center py-3 w-24">
+      <a className="hover:bg-[#008080] hover:text-white hover:rounded-lg text-[#035454] flex justify-center py-3 w-24">
         Friends
       </a>
     </div>
